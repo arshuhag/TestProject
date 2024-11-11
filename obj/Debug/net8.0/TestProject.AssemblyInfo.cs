c0c36e6cbb94c02f6a1144244b3514ab08e658ce
Md. Anisur Rahaman Shuhag <arshuhag.cse@gmail.com>
2024-11-11 21:37:44 +0600

    greading system for 4 students

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92f48813bd9c51d57b3d1bc8b0c68bd52082923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dbb78531b324ff66695eae99c60d4e536e5d01d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
