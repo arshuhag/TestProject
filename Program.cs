e275b0881c636c3062f543111040d587bb877860
Md. Anisur Rahaman Shuhag <arshuhag.cse@gmail.com>
2024-11-11 19:01:26 +0600

    array index current location and sum

--- Program.cs
+++ Program.cs
@@ -1,4 +1,113 @@
-﻿// See https://aka.ms/new-console-template for more information
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+﻿using System;
+
+class Test {
+    public static void Main(String[] args) {
+        string fullName = "Md. Anisur Rahaman";
+        int age = 29;
+        double gpa = 3.67;
+        bool isRagistered = true;
+        char bloodGroup = 'O';
+
+        Console.WriteLine($"Name: {fullName}");
+        Console.WriteLine("Age: "+age);
+        Console.WriteLine("CGPA: "+gpa);
+        Console.WriteLine(isRagistered);
+        Console.WriteLine(bloodGroup);
+
+    }
+}
+//array index current location and sum
+/*
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
+*/
+//subscription expire and discount
+/*
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
+}
+}
+*/
+        //random dice roll with if else
+        /*
+        Random dice = new Random();
+
+        int roll1 = dice.Next(1, 7);
+        int roll2 = dice.Next(1, 7);
+        int roll3 = dice.Next(1, 7);
+
+        int total = roll1 + roll2 + roll3;
+
+        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+        {
+            if (roll1 == roll2 && roll2 == roll3)
+            {
+                Console.WriteLine("You rolled triples!  +6 bonus to total!");
+                total += 6;
+            }
+            else
+            {
+                Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+                total += 2;
+            }
+
+            Console.WriteLine($"Your total including the bonus: {total}");
+        }
+
+        if (total >= 16)
+        {
+            Console.WriteLine("You win a new car!");
+        }
+        else if (total >= 10)
+        {
+            Console.WriteLine("You win a new laptop!");
+        }
+        else if (total == 7)
+        {
+            Console.WriteLine("You win a trip for two!");
+        }
+        else
+        {
+            Console.WriteLine("You win a kitten!");
+        } */
+    
